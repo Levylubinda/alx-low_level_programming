abc300261d89185fc349c54c101368b8c7591531
Levylubinda <lubasi223@gmail.com>
2022-12-09 02:30:39 -0800

    Update advanced task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,11 +4,10 @@
 /**
  * main - print exactly "and that piece of arts is useful"
  * Dora korpar, 2015-10-19",
- *
  * Return: Always 0 (success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 59);
+	write(1, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
