7f41abb890e5ff7c835b6b7fd759a6d788d8eb32
Levylubinda <lubasi223@gmail.com>
2022-12-10 10:42:23 -0800

    Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	char c;
 
-        int d;
+	int d;
 
 	c = 'a';
 	d = 0;
@@ -17,12 +17,12 @@ int main(void)
 		(d < 10) {
 			putchar(d + '0');
 			d++;
-         	}
+		}
 	while
 		(c <= 'f') {
 			putchar(c);
 			c++;
-              }
-	putchar('\n');	
+		}
+	putchar('\n');
 	return (0);
 }
