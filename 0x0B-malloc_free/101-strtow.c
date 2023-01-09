e3e1d75b73a2d0ed28974649f9a618cce4063e41
Levylubinda <lubasi223@gmail.com>
2023-01-09 13:17:06 -0800

    updated 10

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,103 +1,100 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+char **_cpy_strtow(char **words, char *str, int word_cnt);
+
 /**
- *  * len - returns length of str
- *   * @str: string to be counted
- *     * Return: length of the string
- **/
-
-int len(char *str)
-
+ * count_words - count words in string
+ * @str: string to count
+ *
+ * Return: number of words in string
+ */
+int count_words(char *str)
 {
-	int len = 0;
+	int i, word_cnt = 0;
 
-	if (str != NULL)
-	{
-		while (str[len])
-			len++;
-	}
-	return (len);
+	/* loop through string, count words */
+	for (i = 0; str[i] != '\0'; i++)
+		if ((i == 0 && str[i] != ' ') || (str[i - 1] == ' ' && str[i] != ' '))
+			word_cnt++;
+	return (word_cnt);
 }
 
 /**
- *  * num_words - counts the number of words in str
- *   * @str: string to be used
- *     * Return: number of words
- **/
-
-int num_words(char *str)
-
-{
-
-	int i = 0, words = 0;
-
-	while (i <= len(str))
-	{
-		if ((str[i] != ' ') && (str[i] != '\0'))
-
-		{
-			i++;
-		}
-		else if (((str[i] == ' ') || (str[i] == '\0')) && i && (str[i - 1] != ' '))
-		{
-			words += 1;
-			i++;
-		}
-		else
-		{
-			i++;
-		}
-	}
-	return (words);
-
-}
-
-/**
- *  * strtow - splits a stirng into words
- *   * @str: string to be splitted
- *     * Return: pointer to the array of splitted words
- **/
-
+ * strtow - split string into words
+ * @str: string to split
+ *
+ * Return: pointer to array of seperated words, NULL on Fail
+ */
 char **strtow(char *str)
-
 {
-	char **split;
-	int i, j = 0, temp = 0, size = 0, words = num_words(str);
+	char **words;
+	int i, row, str_cnt, word_cnt = count_words(str);
 
-	if (words == 0)
+	/* cover error cases */
+	if (str == NULL || *str == '\0' || word_cnt == 0)
 		return (NULL);
-	split = (char **)malloc(sizeof(char *) * (words + 1));
-	if (split != NULL)
-	{
-		for (i = 0; i <= len(str) && words; i++)
-		{
-			if ((str[i] != ' ') && (str[i] != '\0'))
-				size++;
-			else if (((str[i] == ' ') || (str[i] == '\0')) && i && (str[i - 1] != ' '))
+
+	/* allocate word memory, plus null pointer */
+	words = malloc(sizeof(char *) * (word_cnt + 1));
+	if (words == NULL)
+		return (NULL);
+
+	/* allocate letter memory */
+	for (row = 0, str_cnt = 0; row < word_cnt; row++)
+		for (; str[str_cnt] != '\0'; str_cnt++)
+			if (str[str_cnt] != ' ')
 			{
-				split[j] = (char *)malloc(sizeof(char) * size + 1);
-				if (split[j] != NULL)
+				/* take word length */
+				for (i = 0; str[str_cnt + i] != ' ' && str[str_cnt + i] != '\0'; i++)
+					;
+
+				/* allocate for word, plus null-byte */
+				*(words + row) = malloc(sizeof(char) * (i + 1));
+
+				/* check for fail */
+				if (*(words + row) == NULL)
 				{
-					while (temp < size)
-					{
-						split[j][temp] = str[(i - size) + temp];
-						temp++;
-					}
-					split[j][temp] = '\0';
-					size = temp = 0;
-					j++;
-				}
-				else
-				{
-					while (j-- >= 0)
-						free(split[j]);
-					free(split);
+					for (; row > 0; row--)
+						/* free previously allocated rows */
+						free(*(words + (row - 1)));
+					free(words);
 					return (NULL);
 				}
+
+				/* increase str_cnt past newly malloc'ed word */
+				str_cnt += i;
+				/* once word is allocated break str loop to increment row */
+				break;
 			}
-		}
-		split[words] = NULL;
-		return (split);
-	}
-	else
-		return (NULL);
+	return (_cpy_strtow(words, str, word_cnt));
+}
+
+/**
+ * _cpy_strtow - copy words from string to 2D array
+ * @words: 2D array of words
+ * @str: string to parse
+ * @word_cnt: number of words in str / number of rows in words
+ *
+ * Return: pointer to array of copied words
+ */
+char **_cpy_strtow(char **words, char *str, int word_cnt)
+{
+	int i, row, str_cnt;
+
+	for (row = 0, str_cnt = 0; row < word_cnt; row++)
+		for (; str[str_cnt] != '\0'; str_cnt++)
+			if (str[str_cnt] != ' ')
+			{
+				/* copy words from str */
+				for (i = 0; str[str_cnt + i] != ' ' && str[str_cnt + i] != '\0'; i++)
+					words[row][i] = str[str_cnt + i];
+				words[row][i] = '\0';
+
+				/* increase str_cnt past copied word */
+				str_cnt += i;
+				break;
+			}
+	return (words);
 }
